f17d53c41b41d8628a3040836175919e26d8552d
harikrishnan-03 <harikrishnanq11@gmail.com>
2024-12-10 02:39:16 +0000

    Update dummy_test.py

--- healthbuddy/tests/dummy_test.py
+++ healthbuddy/tests/dummy_test.py
@@ -6,5 +6,5 @@ from django.urls import reverse
 def test_dummy_view(client):
     response = client.get(reverse("homePage"))
     assert response.status_code == 200
-    assert b"Features" in response.content
+    assert b"Thank you for trusting us with your health journey" in response.content
     pass
