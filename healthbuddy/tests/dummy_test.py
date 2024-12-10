baf710580f0abd4dccaed9837626c0e631930815
harikrishnan-03 <harikrishnanq11@gmail.com>
2024-12-10 14:41:42 +0000

    Update dummy_test.py

--- healthbuddy/tests/dummy_test.py
+++ healthbuddy/tests/dummy_test.py
@@ -4,7 +4,7 @@ from django.urls import reverse, NoReverseMatch
 
 @pytest.mark.django_db
 def test_dummy_view(client):
-    url = reverse("unitTesting")
+    url = reverse("abcd")
     response = client.get(url)
     assert response.status_code == 200
-    assert response.content == b"Testing Done"
+    assert response.content == b"abcd"
