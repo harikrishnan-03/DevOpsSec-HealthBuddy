b61ec4b15bc5dab0bce2b52f0fdf67418889ac71
harikrishnan-03 <harikrishnanq11@gmail.com>
2024-12-10 02:20:54 +0000

    Update dummy_test.py

--- healthbuddy/tests/dummy_test.py
+++ healthbuddy/tests/dummy_test.py
@@ -4,9 +4,7 @@ from django.test import Client
 
 @pytest.mark.django_db
 def test_dummy_view(client):
-    """
-    Test the basic functionality of the homepage.
-    """
     response = client.get(reverse('homePage')) 
     assert response.status_code == 200
     assert b"Features" in response.content  
+    pass
