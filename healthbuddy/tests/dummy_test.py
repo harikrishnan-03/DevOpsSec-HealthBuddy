34f630365da5dfaddcb13f910b929a8cba96474b
harikrishnan-03 <harikrishnanq11@gmail.com>
2024-12-10 02:09:29 +0000

    Update dummy_test.py

--- healthbuddy/tests/dummy_test.py
+++ healthbuddy/tests/dummy_test.py
@@ -1,9 +1,12 @@
 import pytest
 from django.urls import reverse
 from django.test import Client
-
+from django.test import TestCase
 
 # Dummy test to check basic Django view response
 @pytest.mark.django_db
 def test_dummy_view():
+    response = self.client.get(reverse('homePage'))  
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "Features")  
     pass
